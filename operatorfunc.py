bee87bfffe625f8a19c9413b1d67b46c3f05bcf2
Sam Soltani <soltaniandsoltani@icloud.com>
2022-11-24 09:43:31 +0100

    Add an Operator function

---
+++ operatorfunc.py
@@ -0,0 +1,19 @@
+def inc_it(x):
+    return x + 1
+
+def dec_it(x):
+    return x - 1
+
+
+def operate_func(func, x):
+    result = func(x)
+    return result
+
+
+
+
+
+print(operate_func(inc_it, 5))
+
+
+
