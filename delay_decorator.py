5ef5be8def78071c04c50e0db0ac93622a53feab
Sam Soltani <soltaniandsoltani@icloud.com>
2022-11-27 06:34:16 +0100

    Adding a decorator module

--- delay_decorator.py
+++ delay_decorator.py
@@ -1,9 +1,14 @@
 def delay_decorator(func):
     def wrapper_function():
+        print("Add sth before the function call...")
         func()
+        print("Print sth just after the function call...")
     return wrapper_function
 
 
 @delay_decorator
 def greet():
     print("hello!")
+
+
+greet()
