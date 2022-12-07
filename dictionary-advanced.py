ece4e633863e7040c4ff1cade7ae1821a6a8afc6
Sam Soltani <soltaniandsoltani@icloud.com>
2022-12-07 10:57:14 +0100

    Add the random function

--- dictionary-advanced.py
+++ dictionary-advanced.py
@@ -1,4 +1,19 @@
+import random
+
+
 city_centers = {'tehran': 'tehran',
                 'alborz':'karaj',
                 'north rheine westfalia': 'duesseldorf',
-                'west azerbayejan' : ''} 
+                'west azerbayejan' : 'tabriz'} 
+
+while True:
+    random_generated = random.choice(list(city_centers))
+    print("What is the center of: ",  random_generated)
+    guess = input("Enter the city: ")
+
+    if guess == 'Quit'.lower():
+       print("Goodbye!") 
+    elif guess == city_centers[random_generated]:
+       print("Yes")
+    else:
+        print("Wrong Answer! the correct answer is: " + city_centers[random_generated])
