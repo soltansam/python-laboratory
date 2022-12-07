3e7e55d853a1d0c218de0e987f3911a37531ee96
Sam Soltani <soltaniandsoltani@icloud.com>
2022-12-07 11:20:23 +0100

    Add Strip function

--- dictionary-advanced.py
+++ dictionary-advanced.py
@@ -9,11 +9,11 @@ city_centers = {'tehran': 'tehran',
 while True:
     random_generated = random.choice(list(city_centers))
     print("What is the center of: ",  random_generated)
-    guess = input("Enter the city: ")
+    guess = input("Enter the city: ").strip()
 
     if guess == 'Quit'.lower():
        print("Goodbye!") 
     elif guess == city_centers[random_generated]:
-       print("Yes")
+       print("Yes, that is the correct answer!")
     else:
         print("Wrong Answer! the correct answer is: " + city_centers[random_generated])
