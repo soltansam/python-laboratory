90396b0a1e5caf945cc08a684236e942e344ac52
devGuru <soltaniandsoltani@icloud.com>
2023-01-02 22:18:26 +0100

    Add dictionary empty

---
+++ dictionaries-empty.py
@@ -0,0 +1,12 @@
+my_dictionary = {}
+
+my_dictionary['name']= 'Sam'
+my_dictionary['family']= 'Solomon'
+my_dictionary['age']= '50'
+my_dictionary['job']= 'engineer'
+my_dictionary['city']= 'Hamburg'
+
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+
+print(alien_0['x_position'])
