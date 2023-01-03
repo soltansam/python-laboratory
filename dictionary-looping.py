8a7a781d21ced6ccd19f9000303e8a6584f6e215
devGuru <soltaniandsoltani@icloud.com>
2023-01-03 07:43:35 +0100

    Iterate through a simple dictioary using items()

---
+++ dictionary-looping.py
@@ -0,0 +1,13 @@
+my_dictionary = {
+    'king sweet': 5,
+    'long sweet' : 6,
+    'large sweet' : 3,
+    'mini sweet' : 1
+    }
+
+
+print(my_dictionary)
+
+for k, v in my_dictionary.items():
+    print(f"\nkeys are: {k}")
+    print(f"values are: {v}")
